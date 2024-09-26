6bf4fd65380aca94eecea04a5b13d3f4ae7b7890
hamzi13 <thehamzagames0@gmail.com>
2024-09-26 12:44:44 -0700

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <p></p>
+
+    
+    </div>
+  )
+}
+
+export default Home
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationsPage = ({params}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TransformationsPage
